f4f140dc30089b1a9d835734738deb9478138138
Gino Luiggi Chirinos Plasencia <gchirinos@factorenergia.com>
2020-02-20 18:41:52 +0100

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props {}
+
+interface State {
+  count: number;
+};
+
+class App extends React.Component<Props, State> {
+  state: State = {
+    count: 1
+  };
+
+  render(){
+    const welcome: string = 'Hello world';
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            { welcome }
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  };
 }
 
 export default App;
